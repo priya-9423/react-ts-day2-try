b03aed50dab56d75e729997cb09d0b729268eb08
priya-9423 <gatpriyanka1293@gmail.com>
2019-11-27 12:34:20 +0530

    1 file modified

--- TimerComponet.tsx
+++ TimerComponet.tsx
@@ -1,6 +1,34 @@
 import React, {Component} from 'react';
+import ReactDOM from 'react-dom';
 
 export default class TimerComponet extends Component
 {
+constructor(props)
+{
+  super(props);
+  this.state = {
+    Timer : new Date().toString()
+  }
+}
 
+componentDidMount()
+{
+  setInterval(() => this.UpdateTimer(), 1000)
+}
+
+UpdateTimer()
+{
+  this.setState({
+    Timer: new Date().toString()
+  })
+}
+
+render()
+{
+return(
+<div>
+  Timer: {this.state.Timer.toString()}
+</div>
+)
+};
 } 
